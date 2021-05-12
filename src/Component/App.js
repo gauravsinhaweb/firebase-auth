f7e9ffee8531b6030e7f8f504646c015f64c024f
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-05-12 14:13:45 +0530

    commit

--- src/Component/App.js
+++ src/Component/App.js
@@ -5,25 +5,31 @@ import { AuthProvider } from "../AuthContext";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LogIn from "./Login";
 import Dashboard from "./Dashboard";
-
+import "./App.css";
+import bg from "./bg.jpg";
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
-            <Switch>
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/" component={LogIn} />
-              <Route exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </Router>
-        </div>
-      </Container>
-    </AuthProvider>
+    <>
+      <div className="bg">
+        <img src={bg} alt="" />
+      </div>
+      <AuthProvider>
+        <Container
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: "100vh" }}
+        >
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Router>
+              <Switch>
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/" component={LogIn} />
+                <Route exact path="/dashboard" component={Dashboard} />
+              </Switch>
+            </Router>
+          </div>
+        </Container>
+      </AuthProvider>
+    </>
   );
 }
 
