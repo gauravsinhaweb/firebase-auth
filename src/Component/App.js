3bae4e410c93da9ae16fad373e9b2163a8149c16
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-05-16 05:00:44 +0530

    commit

--- src/Component/App.js
+++ src/Component/App.js
@@ -5,17 +5,12 @@ import { AuthProvider } from "../AuthContext";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LogIn from "./Login";
 import Dashboard from "./Dashboard";
-import "./App.css";
-import bg from "./bg.jpg";
 function App() {
   return (
     <>
-      <div className="bg">
-        <img src={bg} alt="" />
-      </div>
       <AuthProvider>
         <Container
-          className="d-flex align-items-center justify-content-center"
+          className="d-flex align-items-center justify-content-center "
           style={{ minHeight: "100vh" }}
         >
           <div className="w-100" style={{ maxWidth: "400px" }}>
